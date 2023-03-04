b4224244582e92ed52b33a6b29788723885cc922
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-03-03 21:59:30 -0500

    feature: more details

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,4 +1,4 @@
-import { CalendarIcon } from "@heroicons/react/24/outline";
+import { CalendarIcon, MapIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import ImageCarousel from "./ImageCarousel";
 
@@ -8,17 +8,27 @@ const CarCard = () => {
       <ImageCarousel />
       <div className="z-10 flex rounded-b-lg bg-base-100 p-2">
         <div className="flex w-full items-center justify-between">
-          <div className="grid">
+          <div className="grid gap-y-2">
             <span className="text-sm font-medium text-primary">
               Honda, Civic
             </span>
 
             <div className="flex">
-              <CalendarIcon className="w-5" />
-              <span className="text-sm">2012</span>
+              <CalendarIcon className="w-5 text-primary" />
+              <span className="ml-1 text-sm">2012</span>
+            </div>
+            <div className="flex">
+              <MapPinIcon className="w-5 text-primary" />
+              <span className="ml-1 text-sm">Villa lucre</span>
             </div>
           </div>
-          <span className="font-bold">$35, 648</span>
+          <div className="grid gap-y-2 justify-items-end">
+            <span className="font-bold">$35, 648</span>
+            <div className="flex">
+              <MapIcon className="w-5 text-primary" />
+              <span className="ml-1 text-sm">75,555 KM</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
