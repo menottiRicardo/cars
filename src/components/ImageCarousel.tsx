93970302c5315a72dab8dc50eb9bedc9e19cef54
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-03-03 21:16:50 -0500

    dots added

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -35,17 +35,32 @@ const ImageCarousel = () => {
         } `}
       >
         {slides.map((slide, index) => (
-          <img
-            key={slide}
-            src={slide}
-            className="h-full w-full rounded-t-xl bg-cover bg-center duration-100"
-            style={{
-              order: `${getOrder(index, state.pos, slides.length)}`,
-            }}
-          />
+          <>
+            <img
+              key={slide}
+              src={slide}
+              className="h-full w-full rounded-t-xl bg-cover bg-center duration-100"
+              style={{
+                order: `${getOrder(index, state.pos, slides.length)}`,
+              }}
+            />
+          </>
         ))}
       </div>
-
+      <div className="absolute inset-x-0 bottom-2 z-50 flex h-2 w-full items-center justify-center gap-x-2">
+        {slides.map((slide, index) => {
+          const order = getOrder(index, state.pos, slides.length);
+          console.log(state.pos);
+          return (
+            <div
+              key={index}
+              className={`h-2 w-2 rounded-xl ${
+                state.pos === index ? "bg-primary" : "bg-base-300"
+              }`}
+            />
+          );
+        })}
+      </div>
       {/* Left Arrow */}
       <div
         className="absolute top-[50%] left-5 hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white md:group-hover:block"
